8e3e6e0996df705d1e64702dc2df5edbd6a87340
maddoxmadia <mrm00077@mix.wvu.edu>
2026-03-03 12:18:02 -0500

    Update DatabaseProgrammingObjectsMadia.sql

--- Data/DatabaseProgrammingObjectsMadia.sql/DatabaseProgrammingObjectsMadia.sql
+++ Data/DatabaseProgrammingObjectsMadia.sql/DatabaseProgrammingObjectsMadia.sql
@@ -22,3 +22,70 @@ from Team as t
 join ConferenceDivision as cd on t.ConferenceDivisionID = cd.ConferenceDivisionID
 order by cd.Conference, cd.Division, t.TeamName;
 GO
+
+/*User searches for teams using conference name (optional) and division names (optional) 
+To show TeamName, ConferenceName, DivisionName*/
+
+create or alter procedure procGetTeamsByConferenceDivision
+(
+    @ConferenceName nvarchar(50) = null,
+    @DivisionName nvarchar(50) = null
+)
+AS
+begin
+select TeamName, TeamColors, Conference, Division
+from Team as
+inner join ConferenceDivision as cd on t.ConferenceDivisionID = cd.ConferenceDivisionID
+where (@ConferenceName is null or Conference = @ConferenceName)
+and (@DivisionName is null or Division = @DivisionName);
+end
+GO
+
+/*Find teams in my team's division (user optionally provides their team name)
+Find all teams in my team's division
+To show: TeamName, ConferenceName, DivisionName*/
+
+--create or alter procedure procGetTeamsInMyDivision
+-- (
+--     @TeamName nvarchar(50) = null
+-- )
+-- AS
+-- begin
+-- select t.TeamName, cd.Conference, cd.Division
+-- from Team as t
+-- inner join ConferenceDivision as cd on t.ConferenceDivisionID = cd.ConferenceDivisionID
+-- where cd.ConferenceDivisionID = (
+--     select ConferenceDivisionID
+--     from Team
+--     where TeamName = @TeamName
+-- )
+-- or @TeamName is null;
+-- end
+-- GO
+
+
+/*declare @myTeamName nvarchar(50) = 'Pittsburgh Steelers';
+
+select OtherTeam.TeamName
+from Team MyTeam INNER JOIN Team OtherTeam
+on MyTeam.ConferenceDivisionID = OtherTeam.ConferenceDivisionID
+where MyTeam.TeamName = @myTeamName AND*/
+
+create or alter procedure procGetTeamsInMyDivision
+(
+    @TeamName nvarchar(50) = null
+)
+AS
+begin
+    select t.TeamName, cd.Conference, cd.Division
+    from Team as t
+    inner join ConferenceDivision as cd on t.ConferenceDivisionID = cd.ConferenceDivisionID
+    where (@TeamName is null)
+       or (t.ConferenceDivisionID in (
+           select ConferenceDivisionID
+           from Team
+           where TeamName = @TeamName
+       ))
+    order by cd.Conference, cd.Division, t.TeamName;
+end
+GO
