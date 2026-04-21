08a0f4df6298da697878b023b14c3f9e40ddd6a6
maddoxmadia <mrm00077@mix.wvu.edu>
2026-04-20 22:48:34 -0400

    Fix

--- API/get_teams_for_specified_fan.py
+++ API/get_teams_for_specified_fan.py
@@ -1,17 +1,14 @@
 from get_db_connection import get_db_connection
 import pymssql
 
-def get_teams_by_fan_id(
-        fan_id: int
+def get_teams_for_specified_fan(
+    fan_id: int
 ):
-    #with get_db_connection() as conn:
     conn = get_db_connection()
     cursor = conn.cursor(as_dict=True)
-    #cursor.callproc("procGetTeamsByFanID", (fan_id,))
     cursor.execute("exec procGetTeamsByFanID %s", (fan_id,))
     rows = cursor.fetchall()
     conn.close()
-
     results = [
         {
             "TeamName": row["TeamName"],
@@ -21,5 +18,4 @@ def get_teams_by_fan_id(
         }
         for row in rows
     ]
-
     return {"data": results}
