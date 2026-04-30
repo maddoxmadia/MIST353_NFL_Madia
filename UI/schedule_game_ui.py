7e9ab82738e0f85d9c18d7a40aa3fd8625be88fa
maddoxmadia <mrm00077@mix.wvu.edu>
2026-04-30 19:22:49 -0400

    Fix schedule game UI with error handling

--- UI/schedule_game_ui.py
+++ UI/schedule_game_ui.py
@@ -1,26 +1,50 @@
 import streamlit as st
-from fetch_data import post_data
+from fetch_data import post_data, get_data
+from datetime import date, time
 
 def schedule_game_ui():
     st.header("Schedule a Game")
 
-    home_team_id = st.number_input("Home Team ID", min_value=1, step=1)
-    away_team_id = st.number_input("Away Team ID", min_value=1, step=1)
-    game_round = st.selectbox("Game Round", ["Wild Card", "Divisional", "Conference", "Super Bowl"])
-    game_date = st.date_input("Game Date")
-    game_time = st.time_input("Game Start Time")
-    stadium_id = st.number_input("Stadium ID", min_value=1, step=1)
-    nfl_admin_id = st.number_input("NFL Admin ID", min_value=1, step=1)
+    teams_df = get_data("get_all_teams/", {})
+    stadiums_df = get_data("get_all_stadiums/", {})
+
+    if teams_df is None or stadiums_df is None:
+        st.error("Unable to load teams or stadiums. Please try again later.")
+        return
+
+    GAME_ROUNDS = ["Wild Card", "Divisional", "Conference", "Super Bowl"]
+
+    team_options = dict(zip(teams_df["TeamName"], teams_df["TeamID"]))
+    stadium_options = dict(zip(stadiums_df["StadiumName"], stadiums_df["StadiumID"]))
+
+    home_team_name = st.selectbox("Select Home Team", options=team_options.keys())
+    away_team_name = st.selectbox("Select Away Team", options=team_options.keys())
+    stadium_name = st.selectbox("Select Stadium", options=stadium_options.keys())
+    game_round = st.selectbox("Select Game Round", options=GAME_ROUNDS)
+    game_date = st.date_input("Select Game Date", value=date.today())
+    game_start_time = st.time_input("Select Game Start Time", value=time(12, 0))
 
     if st.button("Schedule Game"):
-        input_params = {
-            "home_team_id": int(home_team_id),
-            "away_team_id": int(away_team_id),
-            "game_round": game_round,
-            "game_date": str(game_date),
-            "game_time": str(game_time),
-            "stadium_id": int(stadium_id),
-            "nfl_admin_id": int(nfl_admin_id)
-        }
-        result = post_data("schedule_game/", input_params)
-        st.success(result.get("status_message", "Done!"))
+        if home_team_name == away_team_name:
+            st.warning("Home team and away team cannot be the same. Please select different teams.")
+            return
+
+        home_team_id = team_options[home_team_name]
+        away_team_id = team_options[away_team_name]
+        stadium_id = stadium_options[stadium_name]
+
+        parameters = {}
+        parameters["home_team_id"] = home_team_id
+        parameters["away_team_id"] = away_team_id
+        parameters["game_date"] = game_date.isoformat()
+        parameters["game_start_time"] = game_start_time.strftime("%H:%M:%S")
+        parameters["stadium_id"] = stadium_id
+        parameters["game_round"] = game_round
+        parameters["nfl_admin_id"] = st.session_state.app_user_id
+
+        response = post_data("schedule_game/", parameters)
+
+        if response is not None and "status_message" in response:
+            st.info(response["status_message"])
+        else:
+            st.error("An error occurred while scheduling the game. Please try again.")
